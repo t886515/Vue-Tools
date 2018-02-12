4b720fecc1ab0ad83b3d5064bba46698ec7c8160
Lina <lina_c_yang@homedepot.com>
2018-02-11 20:43:29 -0600

    -added apollo client for graphql service

--- src/main.js
+++ src/main.js
@@ -4,21 +4,25 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Quasar from 'quasar-framework'
-import { QBtn } from 'quasar-framework'
+
 import 'quasar-extras/roboto-font';
 import 'quasar-extras/material-icons';
 import 'quasar-framework/dist/quasar.ios.css';
 
+import apolloProvider from './apollo';
+
 Vue.config.productionTip = false
+
 Vue.use(Quasar)
-Vue.use(QBtn)
 /* eslint-disable no-new */
 
 
 new Vue({
   el: '#app',
+  apolloProvider,
   router,
   components: { App },
   template: '<App/>',
   // props: ['random']
+  // render: h => h(App),
 })
